9b28b58458f17b7e4e3319ccc518eb3520d37fe0
marvindd <49375112+marvindd@users.noreply.github.com>
2020-08-02 21:10:39 +0530

    Add files via upload

---
+++ DL_member.ps1
@@ -0,0 +1,45 @@
+﻿$date = Get-Date -Format dd.MM.yy-hh.mm.ss
+$scriptpath = $PSScriptRoot
+$Distro = gc -Path "$scriptpath\input\email.txt"
+$finalout = @()
+foreach($Distri in $Distro)
+{
+    $email = $Distri
+    $Distribution = Get-DistributionGroup -Identity $email -ErrorAction silentlycontinue
+    if($Distribution -ne $null)
+    {
+        Write-Host "Cheking for the user $Distri" -fore Cyan
+        $member = Get-DistributionGroupMember -Identity $Distri
+        $report = $member.Primarysmtpaddress
+        $finded = $null
+                
+	    . "$scriptpath\module\get-dlmember.ps1"
+	    #finding member of the Distribution group
+        $Global:finded = $null
+        $Global:finded_all = $null
+        $Global:dlcheck = $null
+        #get-dlmember -groupname $report
+        get-dlmember -groupname $Distri
+        $gpmember = $Global:finded
+
+        $obj = new-object psobject
+        $obj | add-member -membertype noteproperty -Name "DistributionEmail" -value $Distribution.Primarysmtpaddress
+        $obj | add-member -membertype noteproperty -Name "Displayname" -value $Distribution.Displayname
+        $obj | add-member -membertype noteproperty -Name "Alias" -Value $Distribution.Alias
+        $obj | add-member -membertype noteproperty -Name "recipienttypedetails" -Value $Distribution.RecipientTypeDetails
+        $obj | add-member -membertype noteproperty -Name "Member" -value $gpmember
+        $finalOut +=$obj
+    }
+    else
+    {
+        Write-Host "Unable to find Distribution group $Distri" -fore DarkRed
+        $obj = new-object psobject
+        $obj | add-member -membertype noteproperty -Name "DistributionEmail" -value $Distri
+        $obj | add-member -membertype noteproperty -Name "Displayname" -value "Unable to find this group"
+        $obj | add-member -membertype noteproperty -Name "Alias" -Value ""
+        $obj | add-member -membertype noteproperty -Name "recipienttypedetails" -Value ""
+        $obj | add-member -membertype noteproperty -Name "Member" -value ""
+        $finalOut +=$obj
+    }
+}
+$finalout | Export-Csv -Path "$scriptpath\output\distribution.csv" -NoTypeInformation
---
+++ get-dlmember.ps1
@@ -0,0 +1,58 @@
+﻿function get-dlmember
+{
+    param(
+            [string[]]$groupname
+         )
+    BEGIN{}
+    PROCESS
+    {
+        foreach($group in $groupname)
+        {
+            if($Global:dlcheck -notcontains $group)
+            {
+                $checking = get-recipient -identity $group
+                if($checking.RecipientTypeDetails -match "Group")
+                {
+                    Write-Host "checking for group" $checking.Primarysmtpaddress
+                    $memberinf = Get-DistributionGroupMember -Identity $checking.Primarysmtpaddress
+                    $memberinfi = $memberinf.Primarysmtpaddress
+                
+                    $Global:dlcheck += $checking.Primarysmtpaddress -split ' '
+                    get-dlmember -groupname  $memberinfi
+                    
+                }
+                else
+                {
+                    Get-Member -users $checking.Primarysmtpaddress
+                }
+            }
+            
+        }
+    }
+    END{}
+}
+
+function get-member
+{
+    param(
+            [string[]]$users
+         )
+
+    BEGIN{}
+    PROCESS
+    {
+        foreach($user in $users)
+        {
+            if($Global:finded_all -notcontains $user)
+            {
+                Write-Host "Checking for " $user
+                $checkinmember = Get-recipient -identity $user
+                $Find = $checkinmember.primarysmtpaddress
+            
+                $Global:finded += $Find +';'
+                $Global:finded_all += $user -split ' '
+            }
+        }
+    }
+    END{}
+}
